af248bab92f4aa1c54cb56167bca5c0a5b416430
kauamorim <kauamorimauricio@gmail.com>
2026-04-02 00:32:48 -0300

    adjusts pom

--- src/main/java/com/example/bankapi/model/Account.java
+++ src/main/java/com/example/bankapi/model/Account.java
@@ -1,14 +1,29 @@
 package com.example.bankapi.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@AllArgsConstructor
 public class Account {
     private String id;
     private int balance;
 
+    public Account() {}
+
+    public Account(String id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
--- src/main/java/com/example/bankapi/model/EventRequest.java
+++ src/main/java/com/example/bankapi/model/EventRequest.java
@@ -1,14 +1,40 @@
 package com.example.bankapi.model;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Setter
-@Getter
 public class EventRequest {
     String type;
     String origin;
     String destination;
     int amount;
 
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
