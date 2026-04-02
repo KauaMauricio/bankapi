0db2af1b5cff51945de404c1f65aa00b3ea82b85
kauamorim <kauamorimauricio@gmail.com>
2026-04-01 21:51:52 -0300

    feat adiciona modelo Account e EventRequest

---
+++ src/main/java/com/example/bankapi/model/Account.java
@@ -0,0 +1,15 @@
+package com.example.bankapi.model;
+
+public class Account {
+    private String id;
+    private int balance;
+
+    public Account(String id, int balance) {
+        this.id = id;
+        this.balance = balance;
+    }
+    public String getId() { return id; }
+    public int getBalance() { return balance; }
+    public void setBalance(int balance) { this.balance = balance; }
+
+}
---
+++ src/main/java/com/example/bankapi/model/EventRequest.java
@@ -0,0 +1,9 @@
+package com.example.bankapi.model;
+
+public class EventRequest {
+    private String type;
+    private String origin;
+    private String destination;
+    private int amount;
+
+}
